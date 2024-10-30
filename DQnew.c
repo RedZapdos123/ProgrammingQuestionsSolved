cc6372e08ddf006c99bd0f76f11f8afc29b32390
RedZapdos123 <xerontitan90@outlook.com>
2024-10-31 01:14:00 +0530

    A C program to find duplicates.

---
+++ DQnew.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+{
+    //Taking input
+    int s = 0, f = 0;
+    printf("Input the array size: ");
+    scanf("%d", &s);
+    int a[s];
+    printf("Input an array: ");
+    for(int k = 0; k < s; k++) {
+        scanf("%d", &a[k]);
+        if(a[k] == 0)
+            f = 1;
+    }
+
+    //Removing the duplicates
+    int i = -1;
+    for(int k = 0; k < s; k++) {
+        if(a[k] == 0 & i == -1)
+            i = k;
+        for(int j = k+1; j < s; j++) {
+            if(a[k] == a[j]) {
+                a[j] = 0;
+            }
+        }
+    }
+
+    //Printing the new array
+    printf("\nThe new array is: ");
+    for(int k = 0; k < s; k++) {
+        if(a[k] != 0 && i != k)
+            printf("%d ", a[k]);
+        else if(i == k && f == 1)
+            printf("%d ", a[k]);
+    }
+    printf("\n");
+    return 0;
+}

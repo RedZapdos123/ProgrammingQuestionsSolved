e6b6821c3cb0e54501c489f7e3c9fe3eb8fb5e38
RedZapdos123 <xerontitan90@outlook.com>
2024-10-31 08:54:33 +0530

    Added some more C files.

---
+++ StringWordsSwapper.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int s = 1;
+    char str[255];
+    printf("Input a sentence: ");
+    fgets(str, 255, stdin);
+
+    //Counting the number of words
+    for(int j = 0; j < strlen(str); j++){
+        if(str[j] == ' '){
+            s++;
+        }
+    }
+    
+    char words[s][255];
+    int v = 0, z = 0;
+    
+    //Splitting the sentence into words
+    for(int k = 0; k < strlen(str); k++){
+        if(str[k] == ' ' || str[k] == '\n'){
+            words[v][z] = '\0';
+            z = 0;
+            v++;
+        }
+        else{
+            words[v][z] = str[k];
+            z++;
+        }
+    }
+    
+    //Printing the words shifted in a cyclic manner
+    int n = (sizeof(words)/sizeof(words[0]))-1;
+    char temp[255];
+    for(int k = 0; k < s/2; k++){
+        strcpy(temp, words[k]);
+        strcpy(words[k], words[n]);
+        strcpy(words[n], temp);
+        n--;
+    }
+        
+    for(int j = 0; j < s; j++){
+        printf("%s ", words[j]);
+    }
+    
+    
+    return 0;
+}
